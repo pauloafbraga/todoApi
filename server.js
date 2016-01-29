27f93ecae1c06914a07341536ab88af9b0aed639
Paulo Braga <pauloafbraga@gmail.com>
2016-01-29 18:22:31 +0000

    Refactor

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completd: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completd: false
-}, {
-	id: 3,
-	description: 'Wetr to market',
-	completd: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -24,28 +18,57 @@ app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
-// GET / todos/:id
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	//iterate over todos array. Find a match
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			console.log('todoId:' + todoId);
-			console.log('todo.id:' + todo.id);
-			matchedTodo = todo;
-		}
-	});
-	// res.status(404).send();
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
-
 });
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	// trim body.description
+	body.description = body.description.trim();
+
+	// add id field
+	body.id = todoNextId++;
+
+	// push body into array
+	todos.push(body);
+	
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
-	console.log('Express listening on port' + PORT + '!');
+	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
